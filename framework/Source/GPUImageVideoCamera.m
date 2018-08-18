eed84a3954bdba48eb59d0bdb0fee4292e33e22a
Dimitar Milinski <>
2018-08-18 13:10:19 -0500

    Revert test commit

--- framework/Source/GPUImageVideoCamera.m
+++ framework/Source/GPUImageVideoCamera.m
@@ -608,8 +608,6 @@ void setColorConversion709( GLfloat conversionMatrix[9] )
 
 - (void)processVideoSampleBuffer:(CMSampleBufferRef)sampleBuffer;
 {
-    NSLog(@"DIMITAR: PROCESSING VIDEO SAMPLE BUFFER");
-    
     if (capturePaused)
     {
         return;
